c1cde6ba40b9b0171201e9d889de04080e72a3f9
Sergio Gallego Gudiño <sergiogallegogudino@gmail.com>
2023-08-21 19:52:45 +0200

    ejercicios sgallego

---
+++ TA27_1_sgallego/TA27_1_sgallego/Program.cs
@@ -0,0 +1,219 @@
+﻿using System;
+
+namespace Jobs
+{
+    class Jobs
+    {
+        
+        public class Personal
+        {
+
+            private String _Nombre;
+            private double _Sueldo;
+            private double _SueldoNeto;
+            private double _Anual;
+            private double _AnualNeto;
+
+            public double comprobarSueldo(int min, int max, double sueldo)
+            {
+
+                if (!(sueldo > min && sueldo < max))
+                {
+                    throw new Exception("El sueldo no se encuentra dentro los limites establecidos.");
+                }
+
+                return sueldo;
+            }
+
+            public double bonusAnual(double anual)
+            {
+                Console.WriteLine("Bonus anual aplicado");
+                return anual + (anual * 0.1);
+            }
+
+            public String nombre
+            {
+                get { return _Nombre; }
+                set { _Nombre = value; }
+            }
+
+            public double sueldo
+            {
+                get { return _Sueldo; }
+                set { _Sueldo = value; }
+            }
+
+            public double sueldo_neto
+            {
+                get { return _SueldoNeto; }
+                set { _SueldoNeto = value; }
+            }
+
+            public double anual
+            {
+                get { return _Anual; }
+                set { _Anual = value; }
+            }
+            public double anual_neto
+            {
+                get { return _AnualNeto; }
+                set { _AnualNeto = value; }
+            }
+
+            public Personal(String pNombre, double pSueldo) { 
+                nombre = pNombre;
+                sueldo = pSueldo;
+            }
+
+            public class Manager : Personal
+            {
+                public double comprobarSueldo(double pSueldo)
+                {
+                    return base.comprobarSueldo(3000, 5000, sueldo);
+                }
+                public Manager(String pNombre, double pSueldo) : base(pNombre, pSueldo)
+                {
+                    sueldo = pSueldo + (pSueldo * 0.1);
+                    comprobarSueldo(sueldo);
+                    sueldo_neto = sueldo - (sueldo * 0.26);
+                    anual = sueldo * 12;
+                    anual_neto = anual - (anual * 0.26);
+
+                    base.bonusAnual(anual);
+
+                }
+
+            }
+
+            public class Boss : Personal
+            {
+                public double comprobarSueldo(double pSueldo)
+                {
+                    return base.comprobarSueldo(8000, (int)Math.Round(sueldo) + 1, sueldo);
+                }
+                public Boss(String pNombre, double pSueldo) :base(pNombre, pSueldo)
+                {
+                    sueldo = pSueldo + (pSueldo * 0.5);
+                    comprobarSueldo(sueldo);
+                    sueldo_neto = sueldo - (sueldo * 0.32);
+                    anual = sueldo * 12;
+                    anual_neto = anual - (anual * 0.32);
+
+                    base.bonusAnual(anual);
+
+                }
+
+            }
+
+            public class Employee : Personal
+            {
+                public Employee(String pNombre, double pSueldo) : base(pNombre, pSueldo)
+                {
+                    sueldo = pSueldo - (pSueldo * 0.15);
+                }
+
+            }
+
+            public class Volunteer : Personal
+            {
+                public double comprobarSueldo(double pSueldo)
+                {
+                    return base.comprobarSueldo(0, 0, sueldo);
+                }
+                public Volunteer(String pNombre, double pSueldo) : base(pNombre, pSueldo)
+                {
+                    sueldo = 0;
+                    comprobarSueldo(sueldo);
+                    sueldo += 300;
+                    Console.WriteLine("Sueldo: " + sueldo + " ayuda");
+                }
+            }
+
+            public class Junior : Personal
+            {
+                public double comprobarSueldo(double pSueldo)
+                {
+                    return base.comprobarSueldo(900, 1600, sueldo);
+                }
+                public Junior(String pNombre, double pSueldo) : base(pNombre, pSueldo)
+                {
+                    sueldo = pSueldo - (pSueldo * 0.05);
+                    comprobarSueldo(sueldo);
+                    sueldo_neto = sueldo - (sueldo * 0.02);
+                    anual = sueldo * 12;
+                    anual_neto = anual - (anual * 0.02);
+
+                    base.bonusAnual(anual);
+                }
+            }
+
+            public class Mid: Personal
+            {
+                public double comprobarSueldo(double pSueldo)
+                {
+                    return base.comprobarSueldo(1800, 2500, sueldo);
+                }
+                public Mid(String pNombre, double pSueldo) : base(pNombre, pSueldo)
+                {
+                    sueldo = pSueldo - (pSueldo * 0.10);
+                    comprobarSueldo(sueldo);
+                    sueldo_neto = sueldo - (sueldo * 0.15);
+                    anual = sueldo * 12;
+                    anual_neto = anual - (anual * 0.15);
+
+                    base.bonusAnual(anual);
+                }
+            }
+
+            public class Senior : Personal
+            {
+                public double comprobarSueldo(double pSueldo)
+                {
+                    return base.comprobarSueldo(2700, 4000, sueldo);
+                }
+                public Senior(String pNombre, double pSueldo) : base(pNombre, pSueldo)
+                {
+                    sueldo = pSueldo - (pSueldo * 0.10);
+                    comprobarSueldo(sueldo);
+                    sueldo_neto = sueldo - (sueldo * 0.24);
+                    anual = sueldo * 12;
+                    anual_neto = anual - (anual * 0.24);
+
+                    base.bonusAnual(anual);
+
+                }
+            }
+
+            static void Main(String[] args)
+            {
+                //MILESTONE 1
+                Personal _Personal = new Personal("Personal1", 100);
+                Manager _Manager = new Manager("Manager1", 3000);
+                Boss _Boss = new Boss("Boss1", 8000);
+                Employee _Employee = new Employee("Employee1", 100);
+                Junior _Junior = new Junior("Junior1", 1000);
+                Mid _Mid = new Mid("Mid1", 2100);
+                Senior _Senior = new Senior("Senior1", 3200);
+
+                Console.WriteLine("Comparacion de sueldos: ");
+                Console.WriteLine("Personal: " + _Personal.sueldo);
+                Console.WriteLine("Boss: " + _Boss.sueldo);
+                Console.WriteLine("Employee: " + _Employee.sueldo);
+                Console.WriteLine("Manager: " + _Manager.sueldo);
+
+                //MILESTONE 2
+                Console.WriteLine("Junior: " + _Junior.sueldo);
+                Console.WriteLine("Mid: " + _Mid.sueldo);
+                Console.WriteLine("Senior: " + _Senior.sueldo);
+
+                //Descomentar para comprobar excepcion
+                /*Volunteer _Volunteer = new Volunteer("Volunteer1", 100);
+                Console.WriteLine("Volunteer: " + _Volunteer.sueldo);*/ 
+
+                //MILESTONE 3
+                Console.WriteLine("Manager: " + _Manager.sueldo);
+                Console.WriteLine("Manager: " + _Manager.anual);
+            }
+        }
+    }
+}
---
+++ TA27_2_sgallego/TA27_2_sgallego/Program.cs
@@ -0,0 +1,384 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Security.Cryptography.X509Certificates;
+using System.Text.RegularExpressions;
+
+namespace Vehicles
+{
+    public class Vehicle
+    {
+        private Titular _Titular;
+        private List<Persona> _Personas;
+        private String _Matricula;
+        private String _Marca;
+        private String _Color;
+        private String _MarcaDelanteras;
+        private String _MarcaTraseras;
+        private double _DiametroDelanteras;
+        private double _DiametroTraseras;
+
+        public String matricula
+        {
+            get { return _Matricula; }
+            set { _Matricula = value; }
+        }
+
+        public String marca
+        {
+            get { return _Marca; }
+            set { _Marca = value; }
+        }
+        public String color
+        {
+            get { return _Color; }
+            set { _Color = value; }
+        }
+        public String marca_delanteras
+        {
+            get { return _MarcaDelanteras; }
+            set { _MarcaDelanteras = value; }
+        }
+
+        public String marca_traseras
+        {
+            get { return _MarcaTraseras; }
+            set { _MarcaTraseras = value; }
+        }
+
+        public double diametro_delanteras
+        {
+            get { return _DiametroDelanteras; }
+            set { _DiametroDelanteras = value; }
+        }
+
+        public double diametro_traseras
+        {
+            get { return _DiametroTraseras; }
+            set { _DiametroTraseras = value; }
+        }
+
+        public Titular titular
+        {
+            get { return _Titular; }
+            set { _Titular = value; }
+        }
+
+        public List<Persona> personas
+        {
+            get { return _Personas; }
+            set { _Personas = value; }
+        }
+
+        public Vehicle(String pMatricula, String pMarca, String pColor, Titular pTitular, List<Persona> pPersonas) {
+
+            if (Regex.IsMatch(pMatricula, "^[0-9]{4}[A-Za-z]{2,3}$"))
+            {
+                matricula = pMatricula;
+            }
+            else
+            {
+                throw new Exception("El numero de matricula no es válido");
+            }
+            marca = pMarca;
+            color = pColor;
+            titular = pTitular;
+            personas = pPersonas;
+            marca_delanteras = "";
+            diametro_delanteras = 0;
+            marca_traseras = "";
+            diametro_traseras = 0;
+        }
+
+        public void agreagrRuedas()
+        {
+            Console.WriteLine("Introduce la marca de las ruedas delanteras: ");
+            String marcaDelanteras = Console.ReadLine();
+            Console.WriteLine("Introduce el diametro de las ruedas delanteras: ");
+            double diametroDelanteras = double.Parse(Console.ReadLine());
+
+            if (diametroDelanteras < 0.4 || diametroDelanteras > 4)
+            {
+                throw new Exception("El diametro no es válido");
+            }
+
+            marca_delanteras = marcaDelanteras;
+            diametro_delanteras = diametroDelanteras;
+
+            Console.WriteLine("Introduce la marca de las ruedas traseras: ");
+            String marcaTraseras = Console.ReadLine();
+            Console.WriteLine("Introduce el diametro de las ruedas traseras: ");
+            double diametroTraseras = double.Parse(Console.ReadLine());
+
+            if (diametroTraseras < 0.4 || diametroTraseras > 4)
+            {
+                throw new Exception("El diametro no es válido");
+            }
+
+            marca_traseras = marcaTraseras;
+            diametro_traseras = diametroTraseras;
+
+        }
+
+    }
+    public class Coche : Vehicle
+    {
+        public Coche(String pMatricula, String pModelo, String pColor, Titular pTitular, List<Persona> pPersonas) : base(pMatricula, pModelo, pColor, pTitular, pPersonas) { }
+    }
+
+    public class Camion : Vehicle
+    {
+        public Camion(String pMatricula, String pModelo, String pColor, Titular pTitular, List<Persona> pPersonas) : base(pMatricula, pModelo, pColor, pTitular, pPersonas) { }
+    }
+
+    public class Moto : Vehicle
+    {
+        int numDelanteras;
+        int numTraseras;
+        public Moto(String pMatricula, String pModelo, String pColor, int pNumDelanteras, int pNumTraseras, Titular pTitular, List<Persona> pPersonas) : base(pMatricula, pModelo, pColor, pTitular, pPersonas) { 
+        
+            numDelanteras = pNumDelanteras;
+            numTraseras = pNumTraseras;
+        
+        }
+    }
+
+    public class Persona
+    {
+        private String _Nombre;
+        private String _Apellidos;
+        private String _Nacimiento;
+        private Licencia _Licencia;
+
+        public String nombre
+        {
+            get { return _Nombre; }
+            set { _Nombre = value; }
+        }
+
+        public String apellidos
+        {
+            get { return _Apellidos; }
+            set { _Apellidos = value; }
+        }
+
+        public String nacimiento
+        {
+            get { return _Nacimiento; }
+            set { _Nacimiento = value; }
+        }
+
+        public Licencia licencia
+        {
+            get { return _Licencia; }
+            set { _Licencia = value; }
+        }
+
+        public Persona(String pNombre, String pApellidos, String pNacimiento, Licencia pLicencia)
+        {
+            nombre = pNombre;
+            apellidos = pApellidos;
+            nacimiento = pNacimiento;
+            licencia = pLicencia;
+        }
+
+    }
+
+    public class Licencia
+    {
+        private int _Id;
+        private String _TipoLicencia;
+        private String _NombreCompleto;
+        private String _Caducidad;
+
+        public int id
+        {
+            get { return _Id; }
+            set { _Id = value; }
+        }
+        public String tipo_licencia
+        {
+            get { return _TipoLicencia;  }
+            set { _TipoLicencia  = value; }
+        }
+        public String nombre_completo
+        {
+            get { return _NombreCompleto;  }
+            set { _NombreCompleto = value; }
+        }
+        public String caducidad
+        {
+            get { return _Caducidad; }
+            set { _Caducidad = value; }
+        }
+        public Licencia(int pId, String pTipoLicencia, String pNombreCompleto, String pCaducidad)
+        {
+            id = pId;
+            tipo_licencia = pTipoLicencia;
+            nombre_completo = pNombreCompleto;
+            caducidad = pCaducidad;
+        }
+    }
+
+    public class Titular : Persona
+    {
+        public Titular(String pNombre, String pApellidos, String pNacimiento, Licencia pLicencia): base(pNombre, pApellidos, pNacimiento, pLicencia)
+        {
+
+        }
+    }
+
+    public class Conductor : Persona
+    {
+        private Boolean _Aseguranza;
+        private Boolean _Garaje;
+
+        public Boolean aseguranza
+        {
+            get { return _Aseguranza; }
+            set { _Aseguranza = value; }
+        }
+
+        public Boolean garaje
+        {
+            get { return _Garaje; }
+            set { _Garaje = value; }
+        }
+        public Conductor(String pNombre, String pApellidos, String pNacimiento, Licencia pLicencia, Boolean pAseguranza, Boolean pGaraje) : base(pNombre, pApellidos, pNacimiento, pLicencia)
+        {
+            aseguranza = pAseguranza;
+            garaje = pGaraje;
+        }
+    }
+
+    public class Program {
+
+        static void Main(String[] args)
+        {
+            //Seleccion de usuario
+            List<Persona> listaPersonas = new List<Persona>();
+            List<Vehicle> listaVehiculos = new List<Vehicle>();
+            Boolean acabar = false;
+            String tipoVehiculo;
+
+            while (!acabar)
+            {
+                Console.WriteLine("Qué quieres crear (Usuario / Vehiculo) : ");
+                String eleccion = Console.ReadLine();
+
+                if (eleccion == "Usuario")
+                {
+
+                    Console.WriteLine("LICENCIA");
+                    Console.WriteLine("Introduce el identificador: ");
+                    int id = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Introduce el tipo de licencia: ");
+                    String tipo = Console.ReadLine();
+                    Console.WriteLine("Introduce el nombre completo del titular de la licencia: ");
+                    String nombreCompleto = Console.ReadLine();
+                    Console.WriteLine("Introduce su fecha de caducidad: ");
+                    String caducidad = Console.ReadLine();
+
+                    Licencia l1 = new Licencia(id, tipo, nombreCompleto, caducidad);
+
+                    Console.WriteLine("USUARIO");
+                    Console.WriteLine("Introduce el nombre de usuario: ");
+                    String nombre = Console.ReadLine();
+                    Console.WriteLine("Introduce los apellidos del usuario: ");
+                    String apellidos = Console.ReadLine();
+                    Console.WriteLine("Introduce su fecha de nacimiento: ");
+                    String nacimiento = Console.ReadLine();
+
+                    Titular p1 = new Titular(nombre, apellidos, nacimiento, l1);
+                    listaPersonas.Add(p1);
+                }
+                else if (eleccion == "Vehiculo")
+                {
+                    //Asignar usuarios al vehiculo
+                    Console.WriteLine("LICENCIA");
+                    Console.WriteLine("Introduce el identificador: ");
+                    int id = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Introduce el tipo de licencia: ");
+                    String tipo = Console.ReadLine();
+                    Console.WriteLine("Introduce el nombre completo del titular de la licencia: ");
+                    String nombreCompleto = Console.ReadLine();
+                    Console.WriteLine("Introduce su fecha de caducidad: ");
+                    String caducidad = Console.ReadLine();
+
+                    Licencia l1 = new Licencia(id, tipo, nombreCompleto, caducidad);
+
+                    Console.WriteLine("USUARIO");
+                    Console.WriteLine("Introduce el nombre de usuario: ");
+                    String nombre = Console.ReadLine();
+                    Console.WriteLine("Introduce los apellidos del usuario: ");
+                    String apellidos = Console.ReadLine();
+                    Console.WriteLine("Introduce su fecha de nacimiento: ");
+                    String nacimiento = Console.ReadLine();
+
+                    Titular p1 = new Titular(nombre, apellidos, nacimiento, l1);
+                    listaPersonas.Add(p1);
+                    //Añadir vehiculo
+                    Console.WriteLine("Selecciona un vehiculo (Coche/Moto/Camion): ");
+                    String vehiculo = Console.ReadLine();
+                    Console.WriteLine("Introduce la matricula: ");
+                    String matricula = Console.ReadLine();
+                    Console.WriteLine("Introduce la marca: ");
+                    String marca = Console.ReadLine();
+                    Console.WriteLine("Introduce el color: ");
+                    String color = Console.ReadLine();
+                    Vehicle v1;
+                    if (vehiculo == "Coche")
+                    {
+                        v1 = new Coche(matricula, marca, color, p1, listaPersonas);
+                        tipoVehiculo = "Coche";
+                    }
+                    else if (vehiculo == "Camion")
+                    {
+                        v1 = new Coche(matricula, marca, color, p1, listaPersonas);
+                        tipoVehiculo = "Camion";
+                    }
+                    else if (vehiculo == "Moto")
+                    {
+                        Console.WriteLine("Indica el numero de ruedas delanteras: ");
+                        int numDelanteras = int.Parse(Console.ReadLine());
+                        Console.WriteLine("Indica el numero de ruedas traseras: ");
+                        int numTraseras = int.Parse(Console.ReadLine());
+                        v1 = new Moto(matricula, marca, color, numDelanteras, numTraseras, p1, listaPersonas);
+                        tipoVehiculo = "Moto";
+                    }
+                    else
+                    {
+                        throw new Exception("Vehiculo o Tipo de licencia no válido");
+                    }
+                    v1.agreagrRuedas();
+                    listaVehiculos.Add(v1);
+                    Console.WriteLine("INFORMACION DEL VEHICULO: ");
+                    Console.WriteLine("Matricula: " + v1.matricula + " Marca: " + v1.marca + " Color: " + v1.color + " Delanteras: " + v1.marca_delanteras + " " + v1.diametro_delanteras + " Traseras: " + v1.marca_traseras + " " + v1.diametro_traseras);
+                }
+                else
+                {
+                    Console.WriteLine("Comando no válido, inténtalo de nuevo");
+                }
+
+                Console.WriteLine("Quieres continuar? (S/N)");
+                String decision = Console.ReadLine();
+                if (decision == "N")
+                {
+                    acabar = true;
+                }
+
+            }
+            Console.WriteLine("Personas almacenadas:");
+            foreach (Persona persona in listaPersonas)
+            {
+                Console.WriteLine($"Nombre: {persona.nombre}, Apellidos: {persona.apellidos}");
+            }
+
+            Console.WriteLine("Vehículos almacenados:");
+            foreach (Vehicle vehiculo in listaVehiculos)
+            {
+                Console.WriteLine($"Matrícula: {vehiculo.matricula}, Marca: {vehiculo.marca}");
+            }
+        }
+    }
+}
